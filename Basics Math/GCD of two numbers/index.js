fe00bdc990a7f9272f0fad4498e00b755998a5ad
Kuldeep Sharma <sharmakd2736@gmail.com>
2025-08-14 21:43:06 +0530

    GCD of two number with Otpimal approach

--- Basics Math/GCD of two numbers/index.js
+++ Basics Math/GCD of two numbers/index.js
@@ -1,6 +1,7 @@
 function GCD(n1, n2) {
         let gcd = 1;
-        for(let i=2; i<=Math.min(n1, n2); i++){
+        for(let i=2; i<=Math.min(n1, n2); i++){  
+            /* in this if we run loop in reverse then break the if condition where we get i which divide both n1 && n2. It can be more optimal solution for this.*/
             if(n1%i === 0 && n2%i=== 0){
                 gcd = i;
             }
